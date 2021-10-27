fe4837b131b1f9c835e44bbc6863de91778b21a1
David Hranicky <djh30000@gmail.com>
2021-10-27 19:23:48 -0400

    Fixed checking for no history.txt file

--- shell.c
+++ shell.c
@@ -52,14 +52,10 @@ typedef struct History{
     int size;
 } History; 
 
-typedef struct Processes{
-    int processPIDS[BUFFERSIZE];
-    pid_t size;
-} Proceses;
 
 //function prototypes
 Directory *initDir();
-void initHistory();
+void initHistory(int readFromFile);
 char *getCommands(); // Grab commands from standard output and parse them
 Command *parseCommand(); // Grab line, tokenize
 void executeCommand(Command *pcommand);
@@ -79,7 +75,6 @@ void addtohistory(Command *pcommand);
 // Global access variables
 Directory *dirinfo; //init current directory
 History *commandHistory;
-Proceses activeProcesses = malloc(sizeof(activeProcesses));
     
 
 
@@ -105,7 +100,7 @@ void initHistory(int readFromFile){
     FILE *ptr;
     ptr = fopen("history.txt", "r");
 
-    if(ptr){
+    if(ptr != NULL){
         while(1){
             Command *pcommand = malloc(sizeof(Command));
             
@@ -124,8 +119,9 @@ void initHistory(int readFromFile){
 
             addtohistory(pcommand);
         }
+        fclose(ptr);
     }
-    fclose(ptr);
+    
 
     return;
 }
@@ -318,7 +314,7 @@ void start(Command *pcommand){
     //pids
     pid_t parent = getpid();
     pid_t pid = fork();
-    activeProcesses.processPIDS[activeProcesses.size++] = pid;
+
     //construct command string
     char *cmdString[] = {program, NULL};
     if(pid == -1) {
@@ -336,28 +332,6 @@ void start(Command *pcommand){
     return;
 }
 void background(Command *pcommand){
-    char program[BUFFERSIZE];
-    strcpy(program, pcommand->parameters[0]);
-
-    //pids
-    pid_t parent = getpid();
-    pid_t pid = fork();
-    activeProcesses.processPIDS[activeProcesses.size++] = pid;
-    //construct command string
-    char *cmdString[] = {program, NULL};
-    if(pid == -1) {
-        printf("Error starting process.\n");
-    } else if(pid > 0){
-        printf("I am the parent of this child!\n");
-
-    } else {
-        if(DEBUGMODE){
-            printf("Starting... %s\n\n", program);
-        }
-        setpgid(0,0);
-        execv(program, cmdString);
-    }
-    
     return;
 }
 void dalek(Command *pcommand){
@@ -367,9 +341,6 @@ void repeat(Command *pcommand){
     return;
 }
 void dalekall(){
-    for(int i =0; i < activeProcesses.size; i++){
-        
-    }
     return;
 }
 void addtohistory(Command *pcommand){
